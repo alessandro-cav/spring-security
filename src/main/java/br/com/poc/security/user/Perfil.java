c521d2c33a3d83c9afd242e48cb14756a594b282
alessandro <alessandro-cav@hotmail.com>
2023-05-14 12:29:34 -0300

    Criando perfil para testar no lugar do enun

---
+++ src/main/java/br/com/poc/security/user/Perfil.java
@@ -0,0 +1,29 @@
+package br.com.poc.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_perfil")
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Override
+    public String getAuthority() {
+        return this.nome;
+    }
+
+}
