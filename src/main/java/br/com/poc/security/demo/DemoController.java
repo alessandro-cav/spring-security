7568459c2dc6633a21cc0f899364cc23042e5c65
alessandro <alessandro-cav@hotmail.com>
2023-05-14 11:55:33 -0300

    Formas de acessar os dados no authentication

--- src/main/java/br/com/poc/security/demo/DemoController.java
+++ src/main/java/br/com/poc/security/demo/DemoController.java
@@ -1,9 +1,12 @@
 package br.com.poc.security.demo;
 
 
+import br.com.poc.security.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,19 @@ public class DemoController {
 
     @GetMapping
     public ResponseEntity<String> sayHello(Authentication authentication){
-        System.out.println(authentication.toString());
+
+        Object autenticacao = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("autenticacao: " + autenticacao + "\n");
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("Principal: " + principal + "\n");
+
+        User userDetails = (User) authentication.getPrincipal();
+        System.out.println("User nome " + userDetails.getNome() + "\n");
+
+        User usuario = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("Usuario  " + userDetails.getNome());
+
         return ResponseEntity.ok("hello from segured endpoint");
     }
 
